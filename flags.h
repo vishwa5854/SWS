66837d55791d007fb45a1875447598e0a850143d
lgembarz <lucas@DESKTOP-NOLVPF7.localdomain>
2022-11-12 14:04:11 -0500

    init and starting flag parsing

---
+++ flags.h
@@ -0,0 +1,14 @@
+#ifndef FLAGS_H
+#define FLAGS_H
+
+// define struct for flags, with each flag being a single bit
+struct flags_struct {
+    unsigned int c_flag : 1;
+    unsigned int d_flag : 1;
+    unsigned int h_flag : 1;
+    unsigned int i_flag : 1;
+    unsigned int l_flag : 1;
+    unsigned int p_flag : 1;
+}
+
+#endif /* FLAGS_H */
---
+++ sws.c
@@ -0,0 +1,41 @@
+#include <stdlib.h>
+#include <unistd.h>
+#include "flags.h"
+
+int main(int argc, char **argv) {
+
+    // Create new flags struct, initializing all flags to 0
+    struct flags_struct flags = {0};
+    
+    // default flags
+
+    while ((opt = getopt(argc, argv, "c:dhi:l:p:")) != -1) {
+        switch (getopt_ret) {
+            case 'c':
+                flags.c_flag = 1;
+                break;
+            case 'd':
+                flags.c_flag = 1;
+                break;
+            case 'h':
+                flags.c_flag = 1;
+                break;
+            case 'i':
+                flags.c_flag = 1;
+                break;
+            case 'l':
+                flags.c_flag = 1;
+                break;
+            case 'p':
+                flags.c_flag = 1;
+                break;
+            case '?':
+                return EXIT_FAILURE;
+        }
+    }
+
+    // running as a daemon should be its own func, due to -d
+    // logging should be its own func, due to -l
+
+    return EXIT_SUCCESS;
+}

3ee0fb4126dabad0bab17d8897c865fc0a6a213b
RealOkabe <anmolzagrawal@gmail.com>
2022-11-14 06:46:37 -0500

    Added code to access any user's sws directory
    Now the getuserdir will get a username/requested/content string and resolve it to /home/<username>/sws/requested/content and call readdirs on that
    This is the expected behavior for that.

--- getuserdir.c
+++ getuserdir.c
@@ -12,16 +12,32 @@
 
 #include "readdirs.h"
 
-char** getuserdir() {
+char** getuserdir(char* userstr) {
+	int userstrlen = strlen(userstr);
+	char* username;
+	char* requestedContent;
+	int i;
+	for (i = 0; i < userstrlen; i++) {
+		if (userstr[i] == '/') {
+			if ((username = malloc((i + 1) * sizeof(char*))) == NULL)
+				return(NULL);
+			if ((requestedContent = malloc((userstrlen - i - 1) * sizeof(char*))) == NULL)
+				return(NULL);
+			(void) strncpy(username, userstr, i);
+			(void) strncpy(requestedContent, userstr + i + 1, userstrlen - i - 1);
+			break;
+		}
+	}
 	struct passwd *p;
-	if ((p = getpwuid(getuid())) == NULL)
+	if ((p = getpwnam(username)) == NULL)
 		return(NULL);
-	int swsdirlen = strlen(p->pw_dir) + SWS_LEN;
+	int swsdirlen = strlen(p->pw_dir) + SWS_LEN + userstrlen - i - 1;
 	char* swsdir;
 	if ((swsdir = malloc(swsdirlen * sizeof(char*))) == NULL)
 		return(NULL);
 	(void) strncpy(swsdir, p->pw_dir, strlen(p->pw_dir));
-	(void) strncat(swsdir, "/sws", SWS_LEN);
+	(void) strncat(swsdir, "/sws/", SWS_LEN);
+	(void) strncat(swsdir, requestedContent, userstrlen - i - 1);
 	char** dirs = readdirs(swsdir);	
 	return(dirs);
 }

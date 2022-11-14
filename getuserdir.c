0275f6d19edae73de5e5c543710c87043dba550a
RealOkabe <anmolzagrawal@gmail.com>
2022-11-14 05:17:33 -0500

    Added code to get user directory using getpwuid
    We only need to serve contents of the sws directory of the user running the server instead of just any user

--- getuserdir.c
+++ getuserdir.c
@@ -1,39 +1,27 @@
-#define HOME_DIR_LEN	10
+/* Length of the string /sws + 1 for safety */
+#define SWS_LEN 5
 
 #include <dirent.h>
 #include <stdio.h>
 #include <stdlib.h>
 #include <errno.h>
 #include <limits.h>
+#include <pwd.h>
 #include <string.h>
 #include <unistd.h>
 
 #include "readdirs.h"
 
-char** getuserdir(char* username) {
-	int length = strlen(username);
-	char* user;
-	int count = 0;
-	while (count < length) {
-		if (username[count] == '/') {
-			/* Weirdly, when tried with char user[len] it always had an @ in the end so malloc it is */
-			if ((user = malloc((count + 1) * sizeof(char*))) == NULL) {
-				perror("Count not allocate memory");
-				return(NULL);
-			}
-			(void) strncpy(user, username, count + 1);
-			break;
-		}
-		count++;
-	}
-	if (count == length) {
+char** getuserdir() {
+	struct passwd *p;
+	if ((p = getpwuid(getuid())) == NULL)
 		return(NULL);
-	}
-	char swsdir[HOME_DIR_LEN + strlen(user)];
-	(void) strncpy(swsdir, "/home/", strlen("/home/"));
-	(void) strncat(swsdir, user, strlen(user));
-	(void) strncat(swsdir, "sws", strlen("sws"));
+	int swsdirlen = strlen(p->pw_dir) + SWS_LEN;
+	char* swsdir;
+	if ((swsdir = malloc(swsdirlen * sizeof(char*))) == NULL)
+		return(NULL);
+	(void) strncpy(swsdir, p->pw_dir, strlen(p->pw_dir));
+	(void) strncat(swsdir, "/sws", SWS_LEN);
 	char** dirs = readdirs(swsdir);	
 	return(dirs);
 }
-
